a71aedb11dc8ee3b10639d6f25a23d692ed2d10a
Agile Alex <agilealex@gmail.com>
2019-01-08 11:00:34 +0000

    Implemented & tested 'Scenario: game_over. Thus Full Game is Implemented'

--- app.rb
+++ app.rb
@@ -21,8 +21,8 @@ class HangpersonApp < Sinatra::Base
     redirect '/new'
   end
   
+  # Get game index|home page
   get '/new' do
-    #Get game index|home page
     erb :new
   end
   
@@ -38,12 +38,12 @@ class HangpersonApp < Sinatra::Base
   # Use existing methods in HangpersonGame to process a guess.
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
-  #
+  
   # params[:guess].to_s[0] or its equivalent. to_s converts nil to the empty
   # string in case the form field was left blank (and therefore not included in params at all).
   # [0] grabs the first character only; for an empty string, it returns an empty string.
   post '/guess' do
-    letter = params[:guess].to_s[0]
+    letter = params[:guess].to_s[0]  # strange but .to_s not converting here to string.
     if letter.class == NilClass || letter.empty?
       flash[:message] =  "Your input is empty." 
       redirect '/show'
@@ -64,19 +64,22 @@ class HangpersonApp < Sinatra::Base
   # won, lost, or neither, and take the appropriate action.
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
+  
+  # show action that checks whether the game state it is about to show is 
+  # actually a winning or losing state, and if so, it should redirect to the 
+  # appropriate win or lose action.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    redirect '/win' if @game.check_win_or_lose == :win
+    redirect '/lose' if @game.check_win_or_lose == :lose
+    erb :show
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    erb :win
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    erb :lose
   end
   
 end
